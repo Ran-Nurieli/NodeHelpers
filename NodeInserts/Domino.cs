f111502a0fe0ee3df128083aacc26bdefe728a25
User <User@LAB3-3>
2024-01-24 11:24:00 +0200

    aa

---
+++ NodeInserts/Domino.cs
@@ -0,0 +1,28 @@
+﻿using NodeClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeInserts
+{
+    public class Domino
+    {
+        private int num1;
+        private int num2;
+
+        public Domino(int num1,int num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+        public int GetNum1() { return num1; }
+        public int GetNum2() { return num2; }
+        public void SetNum1(int num1) { this.num1 = num1; }
+        public void SetNum2(int num2) {  this.num2 = num2; }
+
+       
+
+    }
+}
