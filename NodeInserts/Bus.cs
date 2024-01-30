700f441fa4a9e64d922a6a1ac0de6d7c4d09bff8
User <User@LAB3-3>
2024-01-30 09:22:45 +0200

    stations

---
+++ NodeInserts/Bus.cs
@@ -0,0 +1,72 @@
+﻿using NodeClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeInserts
+{
+    public class Bus
+    {
+        private int busNum;
+        private int cost;
+        private Node<Station> node;
+        public Bus(int busNum,int cost,Node<Station> node) 
+        {
+            this.busNum = busNum;
+            this.cost = cost;
+            this.node = node;
+        }
+        
+        private int ToPay(Station start,Station end)
+        {
+            if (IsToPay(start, end, node))
+            {
+                return this.cost;
+            }
+            return -1;
+        }
+        private bool IsToPay(Station start,Station end,Node<Station> node)
+        {
+            Node<Station> node2 = node;
+            while(node2 != null)
+            {
+                if(node2.GetValue() ==  start) return true;
+                node2 = node2.GetNext();
+            }
+            return false;
+        }
+
+        public static int BusNumber(CityBuses start,Station end)
+        {
+            if()
+        }
+        public static bool IsFinish(Station end,CityBuses start,Node<Station> node)
+        {
+            Node<Station> node1= node;
+            int countS = 0;
+            int countE = 0;
+            while(node1 != null)
+            {
+                if (node1.GetValue().Equals(start))
+                {
+                    countS++;
+                }
+                if (node1.GetValue().Equals(end))
+                {
+                    countE++;
+                }
+                node1 = node1.GetNext();
+            }
+            if(countS > 0 && countE > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+
+    }
+}
---
+++ NodeInserts/CityBuses.cs
@@ -0,0 +1,16 @@
+﻿using NodeClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeInserts
+{
+     public class CityBuses
+     {
+        private string Name;
+        private Node<Bus> node;
+
+     }
+}
